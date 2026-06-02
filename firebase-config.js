ce507c8e83640a7204588f73cd5d12ff4d07f603
mmprinting3d <mm3dprinting613@gmail.com>
2026-06-02 17:23:35 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,10 @@
 // firebase-config.js
 // Firebase App Initialization Config Module
 export const firebaseConfig = {
-    apiKey: "YOUR_API_KEY_HERE",
-    authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_PROJECT_ID.appspot.com",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID"
+    apiKey: "AIzaSyBEOvl2s_Ysxjgc3f0eWfciFPOj-xESukw",
+    authDomain: "mm-filament-tracker.firebaseapp.com",
+    projectId: "mm-filament-tracker",
+    storageBucket: "mm-filament-tracker.appspot.com",
+    messagingSenderId: "615892017839",
+    appId: "1:615892017839:web:a453f3581248b18a93ea5a"
 };
